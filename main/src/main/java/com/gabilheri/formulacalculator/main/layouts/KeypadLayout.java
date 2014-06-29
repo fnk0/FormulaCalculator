f4ed785b312dd625d76a8ee7d1f08b0fe0fb4f6b
Marcus Gabilheri <marcusandreog@gmail.com>
2014-06-29 01:29:18 -0500

    updates

---
+++ main/src/main/java/com/gabilheri/formulacalculator/main/layouts/KeypadLayout.java
@@ -0,0 +1,20 @@
+package com.gabilheri.formulacalculator.main.layouts;
+
+import android.widget.Button;
+
+/**
+ * Created by <a href="mailto:marcusandreog@gmail.com"> Marcus Gabilheri </a>
+ *
+ * @author Marcus Gabilheri
+ * @version 1.0
+ *          since 6/29/14.
+ */
+public class KeypadLayout {
+
+    private Button btnClear, btnDel, btnParLeft, btnParRight, btnPlus, btnMinus, btnMultiply, btnDivide, btnPercent, btnPower;
+    private Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0, btnDot, btnStr, btnRel, btnEqual;
+    private Button btnSqrt, btnLog,  btnLn, btnSin, btnCos, btnTan, btnE, btnPi, btnInf, btnArcSin, btnArcCos, btnArcTan;
+    
+
+
+}
